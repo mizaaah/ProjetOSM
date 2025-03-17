32f4ecf142dd969dff47940dd924439db34edcc5
mizaaah <enzo.impiglia@isen.yncrea.fr>
2025-03-17 09:34:05 +0100

    Aucun changement

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ document.getElementById('closeIcon').addEventListener('click', function() {
 document.getElementById('openForm').addEventListener('click', function() {
     document.getElementById('formContainer').classList.remove('hidden');
 });
+
