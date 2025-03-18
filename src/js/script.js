c5287fa0b06387059d0a897e6f68ada9ac63f1fb
mizaaah <enzo.impiglia@isen.yncrea.fr>
2025-03-18 16:32:08 +0100

    Changement

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,14 @@ document.querySelector('.oublielien').addEventListener('click', function(event) 
     document.getElementById('registerContainer').classList.add('hidden');
     document.getElementById('mdpOublie').classList.remove('hidden');
 });
+
+// Ajout des événements pour le formulaire de mot de passe oublié
+document.querySelector('#mdpOublie .iconferme').addEventListener('click', function() {
+    document.getElementById('mdpOublie').classList.add('hidden');
+});
+
+document.querySelector('#mdpOublie .oublielien').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('mdpOublie').classList.add('hidden');
+    document.getElementById('formContainer').classList.remove('hidden');
+});
