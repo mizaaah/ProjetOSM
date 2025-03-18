1daa58fbbe22b261f12052ef41ee695b82da9c64
mizaaah <enzo.impiglia@isen.yncrea.fr>
2025-03-18 12:37:09 +0100

    Changement

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.getElementById('openForm').addEventListener('click', function() {
     document.getElementById('formContainer').classList.remove('hidden');
 });
 
+document.querySelector('.incrilien').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('formContainer').classList.add('hidden');
+    document.getElementById('registerContainer').classList.remove('hidden');
+});
+
+document.getElementById('closeRegisterIcon').addEventListener('click', function() {
+    document.getElementById('registerContainer').classList.add('hidden');
+});
