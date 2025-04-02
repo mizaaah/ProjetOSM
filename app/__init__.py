5080a8fecd6e4c7dafbc48e5cd198519800b6a82
mizaaah <enzo.impiglia@isen.yncrea.fr>
2025-04-02 08:55:04 +0200

    Ajout du dossier app

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# filepath: /home/mizaah/Bureau/ProjetOSM/app/__init__.py
+from flask import Flask
+from app.models import db
+from app.routes import routes
+from app.config import Config
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    app.register_blueprint(routes)
+
+    return app
---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+routes = Blueprint('routes', __name__)
+
+@routes.route('/')
+def index():
+    return render_template('index.html')
+
+@routes.route('/accueil')
+def accueil():
+    return render_template('accueil.html')
+
+@routes.route('/admin')
+def admin():
+    return render_template('admin.html')
